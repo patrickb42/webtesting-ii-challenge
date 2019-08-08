ea6078d1d1513bc95e6f15cad29a68f930f454a9
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-08 11:40:49 -0400

    cleaned up files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const app = rtl.render(<App />);
 });
