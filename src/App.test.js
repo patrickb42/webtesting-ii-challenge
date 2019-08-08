0ecb51307e81bc79e57e0e367f1ead26c27ed18b
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-08 14:37:33 -0400

    condensed app test case

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,5 @@ import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 it('renders without crashing', () => {
-  const wrapper = rtl.render(<App />);
-  const element = wrapper.getByText(/test/i);
-  // expect(element).toBeTruthy();
-  // expect(element).toBeInTheDocument();
-  expect(document.body).toContainElement(element);
-  // console.log(expect(document.body));
+  expect(rtl.render(<App />).getByTestId(/^App$/)).toBeInTheDocument();
 });
