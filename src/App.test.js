140dc5bd864ee24a4a8557dea002816362cfc42c
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-08 12:07:39 -0400

    npm test runs

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import * as rtl from 'react-testing-library';
-import 'jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const app = rtl.render(<App />);
+  const element = app.queryByText(/test/i);
+  expect(element).toBeInTheDocument();
 });
