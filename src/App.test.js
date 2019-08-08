5f678b091dda000b21a82db52fe592c5785ab422
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-08 14:25:48 -0400

    testing is wokring

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 it('renders without crashing', () => {
-  const app = rtl.render(<App />);
-  const element = app.queryByText(/test/i);
-  expect(element).toBeInTheDocument();
+  const wrapper = rtl.render(<App />);
+  const element = wrapper.getByText(/test/i);
+  // expect(element).toBeTruthy();
+  // expect(element).toBeInTheDocument();
+  expect(document.body).toContainElement(element);
+  // console.log(expect(document.body));
 });
