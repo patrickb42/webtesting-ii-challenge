498e5bd11d12f5ae76b4c2591811d34702db1670
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-08 22:55:49 -0400

    mvp

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import DashboardButton from '../DashboardButton';
+
 const Dashboard = ({ buttons }) => {
   return (
     <div data-testid="Dashboard">
-
+      {buttons.map((button) => {
+        return <DashboardButton key={`dashboard-button-${button.label}`} {...button} />
+      })}
     </div>
   );
 };
