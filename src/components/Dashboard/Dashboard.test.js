a07758b9b1714c2182fe97c70f89c5b80458e593
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-09 00:46:08 -0400

    removed unnecessary functions

--- src/components/Dashboard/Dashboard.test.js
+++ src/components/Dashboard/Dashboard.test.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent,  } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
 
 const balls = 0;
-const setBalls = () => {};
 const strikes = 0;
-const setStrikes = () => {};
 const outs = 0;
-const setOuts = () => {};
 
 const metrics = [];
 metrics.push({ label: 'Balls', data: balls });
