c6c1ff329184390c71ac7b45830cbe7773321d24
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-08 23:51:31 -0400

    trimmed down dashboard test

--- src/components/Dashboard/Dashboard.test.js
+++ src/components/Dashboard/Dashboard.test.js
@@ -4,8 +4,24 @@ import '@testing-library/jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
 
+const balls = 0;
+const setBalls = () => {};
+const strikes = 0;
+const setStrikes = () => {};
+const outs = 0;
+const setOuts = () => {};
+
+const metrics = [];
+metrics.push({ label: 'Balls', data: balls });
+metrics.push({ label: 'Strikes', data: strikes });
+metrics.push({ label: 'Outs', data: outs });
+
+const buttons = [];
+buttons.push({ label: 'Ball', onClick: setBalls});
+buttons.push({ label: 'Strike', onClick: setStrikes});
+
 describe('<Dashboard />', () => {
-  const { getByTestId } = render(<Dashboard />);
+  const { getByTestId } = render(<Dashboard buttons={buttons} />);
   
   it('renders without crashing', () => {
     getByTestId(/^Dashboard$/);
